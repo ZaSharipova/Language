b291cd0c2d500e1f0ac25a2dd6976e60a0e68a36
Зарина Шарипова <Sh.zarina.ild@gmail.com>
2026-05-05 11:46:06 +0300

    Changed MyDraw logics

--- mylib.asm
+++ mylib.asm
@@ -1182,63 +1182,65 @@ my_exit:
 ;-------------------------------------------
 ;-------------------------------------------
 my_draw:
-    push rbp
-    mov rbp, rsp
-    push r12
-    push r13
-    push r14
+        push rbp
+        mov rbp, rsp
+        push r12
+        push r13
+        push r14
 
-    test rdi, rdi
-    jz .end_err
+        test rdi, rdi
+        jz .end_err
 
-    mov r12, rdi
-    xor r13, r13
+        mov r12, rdi
+        xor r13, r13
 
 .loop:
-    cmp byte [r12 + r13], 0
-    jne .print_plus
+        mov rax, r13
+        shl rax, 3
+        mov rcx, r12
+        sub rcx, rax
+        cmp qword [rcx], 0
+        jne .print_plus
 
 .print_dot:
-    lea rsi, [dot_raw]
-    jmp .do_write
+        lea rsi, [dot_raw]
+        jmp .do_write
 
 .print_plus:
-    lea rsi, [plus_raw]
+        lea rsi, [plus_raw]
 
 .do_write:
-    mov rax, 1
-    mov rdi, 1
-    mov rdx, 2
-    syscall
+        mov rax, 1
+        mov rdi, 1
+        mov rdx, 2
+        syscall
 
 .check_newline:
-    mov rax, r13
-    inc rax
-    and rax, 31
-    jnz .next_iter
+        mov rax, r13
+        inc rax
+        and rax, 31
+        jnz .next_iter
 
-    mov rax, 1
-    mov rdi, 1
-    lea rsi, [nl_raw]
-    mov rdx, 1
-    syscall
+        mov rax, 1
+        mov rdi, 1
+        lea rsi, [nl_raw]
+        mov rdx, 1
+        syscall
 
 .next_iter:
-    inc r13
-    cmp r13, 1024
-    jl .loop
+        inc r13
+        cmp r13, 1024
+        jl .loop
 
-    inc qword [r12 + 1024] 
-
-    xor eax, eax
+        xor eax, eax
 
 .end_err:
-    pop r14
-    pop r13
-    pop r12
-    mov rsp, rbp
-    pop rbp
-    ret
+        pop r14
+        pop r13
+        pop r12
+        mov rsp, rbp
+        pop rbp
+        ret
 
         align 8
 
